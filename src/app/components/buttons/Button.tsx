739787fff26a8b2571970e7178992ad15ee9ffd8
shuding0307 <suhyun2116@gmail.com>
2024-07-08 03:12:54 +0900

    style:Share Button

--- src/app/components/buttons/Button.tsx
+++ src/app/components/buttons/Button.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import styles from './button.module.css';
 
 interface IButtonProps {
---
+++ src/app/components/buttons/ShareButton.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import Button from './Button';
+import Icon_download from '../../../../public/icons/buttons/download.svg';
+import Icon_disabled from '../../../../public/icons/buttons/download_disabled.svg';
+import styles from './share.module.css';
+import Image from 'next/image';
+
+interface IShareIprops {
+  state?: 'default' | 'disabled';
+  onClick?: () => void;
+}
+
+export default function ShareButton({
+  state = 'default',
+  onClick,
+}: IShareIprops) {
+  return (
+    <div className={styles.Container}>
+      <Button state={state} onClick={onClick}>
+        공유하기
+      </Button>
+      <div
+        className={`${styles[state]} ${styles.BtnBox}`}
+        onClick={() => {
+          return;
+        }}
+      >
+        <Image
+          src={state === 'default' ? Icon_download : Icon_disabled}
+          width={24}
+          height={24}
+          alt='share'
+        />
+      </div>
+    </div>
+  );
+}

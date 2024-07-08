1b3d2bd0440042e096e13d8e86c9f166f62f7512
shuding0307 <suhyun2116@gmail.com>
2024-07-08 21:47:04 +0900

    feat: header

---
+++ src/app/myMenu/page.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/Header';
+import styles from './myMenu.module.css';
+
+export default function MyMenu() {
+  return (
+    <div className={styles.Container}>
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import styles from './header.module.css';
+import Image from 'next/image';
+import Logo from '../../public/logo/Logo_noBg_orange.svg';
+import Icon from '../../public/icons/Icon_mypage.svg';
+
+export default function Header() {
+  return (
+    <div className={styles.HeaderContainer}>
+      <Image
+        className={styles.Icon}
+        src={Logo}
+        alt='logo'
+        width={50}
+        height={50}
+        onClick={() => {
+          return;
+        }}
+      />
+      <Image
+        className={styles.Icon}
+        src={Icon}
+        alt='myPage'
+        width={24}
+        height={24}
+        onClick={() => {
+          return;
+        }}
+      />
+    </div>
+  );
+}

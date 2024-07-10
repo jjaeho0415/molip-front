4999b1f53449c7e3a97436d9fafea0c5d9f870e7
shuding0307 <suhyun2116@gmail.com>
2024-07-11 03:12:45 +0900

    투표 완료까지 마무리

--- src/app/vote/components/voteDone.tsx
+++ src/app/vote/components/voteDone.tsx
@@ -3,17 +3,39 @@
 import styles from './voteComponents.module.css';
 import Button from '@/components/buttons/Button';
 import TopNavBar from '@/components/TopNavBar';
+import Image from 'next/image';
+import Icon from '../../../../public/icons/checkOrange.svg';
+import OptionButton from '@/components/buttons/OptionButton';
 
 interface IVoteDoneProps {
 	onNext: () => void;
 }
 
+const votes = ['김치찜', '물냉면', '짬뽕'];
+
 export default function VoteDone({ onNext }: IVoteDoneProps) {
 	return (
 		<>
 			<TopNavBar />
-			<div className={styles.DoneContentsContainer}></div>
-			<div className={styles.ButtonBox}>
+			<div className={styles.DoneContentsContainer}>
+				<div className={styles.DoneContent}>
+					<Image src={Icon} width={48} height={48} alt='check' />
+					<p className={styles.DoneP}>투표를 완료했습니다.</p>
+					<div className={styles.DoneOptionBox}>
+						{votes.map((item, idx) => (
+							<div key={idx}>
+								<OptionButton state='selected' option={false}>
+									{item}
+								</OptionButton>
+							</div>
+						))}
+					</div>
+				</div>
+				<p className={styles.DoneVotingComment}>
+					다른 팀원이 투표 중입니다. (3/4)
+				</p>
+			</div>
+			<div className={styles.DoubleButtonBox}>
 				<Button size='small' onClick={onNext}>
 					다시 선택하기
 				</Button>
--- src/app/vote/components/voteResult.tsx
+++ src/app/vote/components/voteResult.tsx
@@ -7,10 +7,19 @@ import TopNavBar from '@/components/TopNavBar';
 export default function VoteResult() {
 	return (
 		<>
-			<TopNavBar />
-			<div className={styles.ResultContentsContainer}></div>
-			<div className={styles.ButtonBox}>
-				<Button size='big'>투표하기</Button>
+			<TopNavBar title='스위프의 메뉴판' />
+			<div className={styles.ResultContentsContainer}>
+				<div className={styles.ResultTextBox}>
+					<p className={styles.VotingP}>투표 결과</p>
+					<div className={styles.ResultDateBox}>
+						<p className={styles.ResultDate}>2024.07.07</p>
+						<p className={styles.ResultSubRight}>익명 투표</p>
+					</div>
+				</div>
+			</div>
+			<div className={styles.DoubleButtonBox}>
+				<Button size='small'>다시 선택하기</Button>
+				<Button size='small'>지난 결과 보기</Button>
 			</div>
 		</>
 	);

2bdd2b1d9606b558eddca5a111af111ff3e0cb0a
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-07-11 01:12:13 +0900

    Style: 메뉴판 페이지 목록 컴포넌트 작업 완료

--- src/data/menuList.ts
+++ src/data/menuList.ts
@@ -1,21 +1,21 @@
 export const teamMenuList = [
-	{
-		teamName: '스위프 10팀',
-		menuName: '스위프 10팀의 메뉴판',
-		teamNumber: 5,
-	},
-	{
-		teamName: '맛집 동아리 PIG',
-		menuName: '3번째 정기 모임',
-		teamNumber: 6,
-	},
+//	{
+//		teamName: '스위프 10팀',
+//		menuName: '스위프 10팀의 메뉴판',
+//		teamNumber: 5,
+//	},
+//	{
+//		teamName: '맛집 동아리 PIG',
+//		menuName: '3번째 정기 모임',
+//		teamNumber: 6,
+//	},
 ];
 
 export const myMenuList = [
-	{
-		menuName: '매운거 땡길 때',
-	},
-	{
-		menuName: '스위프의 메뉴판',
-	},
+//	{
+//		menuName: '매운거 땡길 때',
+//	},
+//	{
+//		menuName: '스위프의 메뉴판',
+//	},
 ];

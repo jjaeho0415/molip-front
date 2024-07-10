b9b83498f9437f631d633b165fe14198f1316322
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-07-11 01:55:21 +0900

    Feat: 스플래시 페이지 3초 보이고 난 후 로그인 페이지로 이동하도록 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,22 @@
 'use client';
 
-import MenuCardModal from '@/components/modals/MenuCardModal';
-import { useState } from 'react';
-import TrialViewModal from '@/components/modals/TrialViewModal';
-import TopMenuButton from '@/components/buttons/TopMenuButton';
 import Splash from '@/components/Splash';
-import TopNavBar from '@/components/TopNavBar';
-import Button from '@/components/buttons/Button';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
-	const [isMenuCardModalOpen, setIsMenuCardModalOpen] =
-		useState<boolean>(false);
-	const [isTrialModalOpen, setIsTrialModalOpen] = useState<boolean>(false);
+	const route = useRouter();
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			route.push('/login');
+		}, 3000);
+		return () => clearTimeout(timer);
+	}, [route]);
 
 	return (
 		<>
 			<Splash />
-			<TopNavBar title='스위프의 메뉴판' />
-			<TopNavBar menu={true} />
-			<div>
-				<button onClick={() => setIsMenuCardModalOpen(true)}>
-					메뉴카드 모달
-				</button>
-				<button onClick={() => setIsTrialModalOpen(true)}>체험판 모달</button>
-				{isMenuCardModalOpen && (
-					<MenuCardModal
-						menuTitle='감자탕'
-						menuImage='/image/감자탕.jpg'
-						hashTags={['한식', '매콤함', '국물있는', '밥', '추운날']}
-						setIsMenuCardModalOpen={setIsMenuCardModalOpen}
-					/>
-				)}
-				{isTrialModalOpen && (
-					<TrialViewModal setIsTrialModalOpen={setIsTrialModalOpen} />
-				)}
-			</div>
-			<TopMenuButton />
-			<br />
-			<TopMenuButton size='small' />
-			<br />
-			<Button state='new'>+ 새로만들기</Button>
 		</>
 	);
 }
---
+++ src/components/modals/AlertModal.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles/alertModal.module.css'
+
+function AlertModal() {
+  return (
+    <div>AlertModal</div>
+  )
+}
+
+export default AlertModal

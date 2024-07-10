bdd1bd877bf779bdc12c208a1d80076c16c8f56e
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-07-11 02:19:55 +0900

    Style: 확인 버튼 하나만 있는 AlertModal 작업 완료

--- src/components/modals/AlertModal.tsx
+++ src/components/modals/AlertModal.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import styles from './styles/alertModal.module.css'
+import React, { Dispatch, SetStateAction } from 'react';
+import styles from './styles/alertModal.module.css';
+import Image from 'next/image';
+import ModalButton from '../buttons/ModalButton';
 
-function AlertModal() {
-  return (
-    <div>AlertModal</div>
-  )
+interface AlertModalProps {
+	setIsAlertModalOpen: Dispatch<SetStateAction<boolean>>;
+	max: number;
 }
 
-export default AlertModal
+function AlertModal({ setIsAlertModalOpen, max }: AlertModalProps) {
+	return (
+		<>
+			<div
+				className={styles.overlay}
+				onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+					if (event.target === event.currentTarget) {
+						setIsAlertModalOpen(false);
+					}
+				}}
+			>
+				<div className={styles.modal}>
+					<div className={styles.contentSection}>
+						<Image
+							alt='checkIcon'
+							width={48}
+							height={48}
+							src='/svg/checkIcon.svg'
+						/>
+						<p>메뉴는 최대 {max}개까지만</p>
+						<p>선택 가능합니다.</p>
+					</div>
+					<div className={styles.buttonSection}>
+						<ModalButton
+							color='orange'
+							buttonText='확인'
+							handleClick={() => setIsAlertModalOpen(false)}
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default AlertModal;

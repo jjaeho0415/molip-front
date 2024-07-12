9c43c8c06df369481b2160f01b0a8388a23016c1
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-07-12 20:16:50 +0900

    Style: 로그인 페이지 튜토리얼 swiper 퍼블리싱 시작

---
+++ src/app/login/_components/TutorialStep.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './tutorial.module.css'
+
+interface TutorialStepProps{
+    step: number;
+}
+
+function TutorialStep({step}: TutorialStepProps) {
+  return (
+      <>
+        
+      </>
+  )
+}
+
+export default TutorialStep

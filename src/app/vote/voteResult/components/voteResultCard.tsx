b2ed96cfc0f793fcd3cd1a542eb2995c8877524c
shuding0307 <suhyun2116@gmail.com>
2024-07-13 02:12:09 +0900

    지난 결과 보기 페이지 퍼블리싱 & 로그인 페이지 css 수정

---
+++ src/app/vote/voteResult/components/voteResultCard.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import OptionButton from '@/components/buttons/OptionButton';
+import styles from './voteResultCard.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { FreeMode } from 'swiper/modules';
+
+interface IVoteResultType {
+	team: string;
+	date: string;
+	menu: string[];
+}
+
+interface IVoteResultProps {
+	voteResult: IVoteResultType;
+}
+
+export default function VoteResultCard({ voteResult }: IVoteResultProps) {
+	return (
+		<div className={styles.Container}>
+			<div className={styles.TopBox}>
+				<p className={styles.TeamName}>{voteResult.team}</p>
+				<p className={styles.Date}>{voteResult.date}</p>
+			</div>
+			<Swiper
+				spaceBetween={5}
+				slidesPerView={3.5}
+				freeMode={true}
+				modules={[FreeMode]}
+				className={styles.BottomBox}
+			>
+				{voteResult.menu.map((item: string, idx: number) => (
+					<SwiperSlide className={styles.Option} key={idx}>
+						<OptionButton
+							option={false}
+							state={idx === 0 ? 'checked' : 'selected'}
+						>
+							{item}
+						</OptionButton>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+}
---
+++ src/app/vote/voteResult/page.tsx
@@ -0,0 +1,61 @@
+import Header from '@/components/Header';
+import styles from './voteResult.module.css';
+import TabNavigation from '@/components/TabNavigation';
+import TopNavBar from '@/components/TopNavBar';
+import VoteResultCard from './components/voteResultCard';
+
+const voteResults = [
+	{
+		team: '스위프 10팀',
+		date: '2024.07.07(일)',
+		menu: ['물냉면', '비빔냉면', '짬뽕', '쫄면', '짜장면'],
+	},
+	{
+		team: '맛집 동아리',
+		date: '2024.06.21(금)',
+		menu: ['물냉면', '비빔밥', '짬뽕', '쫄면', '짜장면'],
+	},
+	{
+		team: '스위프 10팀',
+		date: '2024.07.07(일)',
+		menu: ['마라전골', '아이스크림', '마라짬ㄱ뽕', '샤브샤브', '짜장면'],
+	},
+	{
+		team: '맛집 동아리',
+		date: '2024.06.21(금)',
+		menu: ['물냉면', '비빔냉면', '짬뽕', '쫄면', '짜장면'],
+	},
+	{
+		team: '스위프 10팀',
+		date: '2024.07.07(일)',
+		menu: ['물냉면', '비빔냉면', '짬뽕', '쫄면', '짜장면'],
+	},
+];
+
+export default function VoteResult() {
+	return (
+		<div className={styles.Container}>
+			<Header />
+			<TabNavigation />
+			<TopNavBar title='지난 결과 보기' />
+			<div className={styles.ContentContainer}>
+				{voteResults ? (
+					<>
+						<div className={styles.CardListBox}>
+							{voteResults.map((voteResult, idx) => (
+								<div key={idx}>
+									<VoteResultCard voteResult={voteResult} />
+								</div>
+							))}
+						</div>
+						<p className={styles.BottomComment}>
+							30일이 지난 투표 결과는 자동으로 삭제됩니다.
+						</p>
+					</>
+				) : (
+					<p className={styles.NoVote}>최근 30일간 투표 내역이 없어요.</p>
+				)}
+			</div>
+		</div>
+	);
+}

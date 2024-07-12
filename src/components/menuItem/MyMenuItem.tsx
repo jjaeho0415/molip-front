997c6cf4b757776d3c58e6c990776ec9c91fb933
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-07-12 22:45:01 +0900

    Refactor: 메뉴판 목록 아이템들 cursor: pointer 속성 추가

--- src/components/menuItem/MyMenuItem.tsx
+++ src/components/menuItem/MyMenuItem.tsx
@@ -4,49 +4,50 @@ import Image from 'next/image';
 import MoreModal from '../modals/MoreModal';
 
 interface MyMenuItemProps {
-  menuTitle: string;
+	menuTitle: string;
 }
 
 function MyMenuItem({ menuTitle }: MyMenuItemProps) {
-  const [isMoreModalOpen, setIsMoreModalOpen] = useState<boolean>(false);
+	const [isMoreModalOpen, setIsMoreModalOpen] = useState<boolean>(false);
 
-  return (
-    <>
-      <div className={styles.itemContainer}>
-        <p className={styles.menuTitle}>{menuTitle}</p>
-        <Image
-          alt='moreIcon'
-          src='/svg/moreIcon.svg'
-          width={24}
-          height={24}
-          onClick={() =>
-            isMoreModalOpen
-              ? setIsMoreModalOpen(false)
-              : setIsMoreModalOpen(true)
-          }
-          style={{ position: 'relative', cursor: 'pointer', zIndex: '1' }}
-        />
-        {isMoreModalOpen && (
-          <>
-            <div
-              style={{
-                position: 'absolute',
-                transform: 'translate(191.5px, 65px)',
-                zIndex: '6',
-              }}
-            >
-              <MoreModal
-                menuTitle={menuTitle}
-                teamNumber={0}
-                teamTitle=''
-                pageType='outsideMyMenu'
-              />
-            </div>
-          </>
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={styles.itemContainer}>
+				<p className={styles.menuTitle}>{menuTitle}</p>
+				<Image
+					alt='moreIcon'
+					src='/svg/moreIcon.svg'
+					width={24}
+					height={24}
+					onClick={(e) => {
+						e.preventDefault();
+						isMoreModalOpen
+							? setIsMoreModalOpen(false)
+							: setIsMoreModalOpen(true);
+					}}
+					style={{ position: 'relative', cursor: 'pointer', zIndex: '1' }}
+				/>
+				{isMoreModalOpen && (
+					<>
+						<div
+							style={{
+								position: 'absolute',
+								transform: 'translate(191.5px, 65px)',
+								zIndex: '6',
+							}}
+						>
+							<MoreModal
+								menuTitle={menuTitle}
+								teamNumber={0}
+								teamTitle=''
+								pageType='outsideMyMenu'
+							/>
+						</div>
+					</>
+				)}
+			</div>
+		</>
+	);
 }
 
 export default MyMenuItem;

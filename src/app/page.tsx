52ae30ee62c8e90a0fda0efb56ba00766bacbed8
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-07-29 04:09:38 +0900

    feat: 로그인 여부에 따른 루트 페이지 리디렉션 작업 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,45 @@
 import Splash from '@/components/Splash';
 import KakaoInitializer from '@/utils/KakaoInitializer';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuthStore } from './login/store/useAuthStore';
+import Loading from '@/components/Loading';
 
-export default function Home() {
+export default function App() {
 	const route = useRouter();
 	const { isLogin } = useAuthStore.getState();
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		const timer = setTimeout(() => {
-			route.push('/login');
-		}, 2000);
-		return () => clearTimeout(timer);
-	}, [route]);
+		if (isLogin) {
+			route.replace('/home');
+		} else {
+			setLoading(false);
+
+			const timer = setTimeout(() => {
+				route.push('/login');
+			}, 2000);
+			return () => clearTimeout(timer);
+		}
+	}, [isLogin, route]);
 
 	return (
 		<>
 			<KakaoInitializer />
-			{}
-			<Splash />
+			{loading ? (
+				<div
+					style={{
+						display: 'flex',
+						height: '100vh',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Loading backgroundColor='white' />
+				</div>
+			) : (
+				<Splash />
+			)}
 		</>
 	);
 }

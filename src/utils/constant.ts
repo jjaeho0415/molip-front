185d1ca80d56489d71f414149ecca10e3acbf8a1
정재로 <91364411+jjaeho0415@users.noreply.github.com>
2024-08-02 17:29:51 +0900

    chore: constant에 protectedRoute 경로 넣어주기

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,14 @@
 export const constant = {
 	kakaoKey: process.env.NEXT_PUBLIC_KAKAOKEY as string,
 	token: process.env.NEXT_PUBLIC_ACCESS as string,
+	protectedRoute: [
+		'/teamMenuPage',
+		'/myPage',
+		'/menu',
+		'/makeTeam',
+		'/kakaoMap',
+		'/home',
+		'/vote',
+		'/createMyMenu',
+	],
 };
